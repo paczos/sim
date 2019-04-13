ca8bdcd47ecb8ac5fb0798d7bd7a5fd2a639cd34
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-04-13 16:05:55 +0200

    fixed print_node method

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include "rapidxml/rapidxml_utils.hpp"
+#include "rapidxml/rapidxml_ext.h"
+
 #include <string>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
 
 using namespace std;
+using namespace rapidxml;
 
 int main() {
     string dcmFilePath;
@@ -21,10 +27,13 @@ int main() {
 
 
     //TODO create Hl7 here
+    xml_document<> doc;
+    xml_node<> *root_node;
 
+    xml_node<> *new_node = doc.allocate_node(node_element, "Beer", "LOL");
 
-
-
+    cout << "Hello :) this is the hl7 message:" << endl;
+    cout << doc << endl;
     string renderingCommand =
             "xalan -xsl transformata_hl7/narrative-block-1.3.1/CDA_PL_IG_1.3.1.xsl -in hl7example.xml -out hl7example.html";
     system(renderingCommand.c_str());
