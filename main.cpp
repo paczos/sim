58d9a78cc25d79a5223af844fa9d802b7b6dab5f
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-04-15 10:43:02 +0200

    naming convention

--- main.cpp
+++ main.cpp
@@ -12,19 +12,19 @@ using namespace std;
 using namespace rapidxml;
 
 int main() {
-    string dcmFilePath;
+    string dcm_file_path;
     cout << "Podaj sciezke do pliku" << endl;
-    cin >> dcmFilePath;
-    string dcmj2pnmCommand;
-    dcmj2pnmCommand = "dcmj2pnm --write-16-bit-png " + dcmFilePath + " photo.png";
-    system(dcmj2pnmCommand.c_str());
+    cin >> dcm_file_path;
+    string dcmj2pnm_command;
+    dcmj2pnm_command = "dcmj2pnm --write-16-bit-png " + dcm_file_path + " photo.png";
+    system(dcmj2pnm_command.c_str());
 
     cout << "Done" << endl;
 
-    string dcm2XmlCommand;
-    cout << dcmFilePath << endl;
-    dcm2XmlCommand = "dcm2xml " + dcmFilePath + " converted.xml";
-    system(dcm2XmlCommand.c_str());
+    string dcm_2_xml_command;
+    cout << dcm_file_path << endl;
+    dcm_2_xml_command = "dcm2xml " + dcm_file_path + " converted.xml";
+    system(dcm_2_xml_command.c_str());
 
     xml_document<> doc;
     ifstream theFile("report_hl7_template.xml");
@@ -41,15 +41,15 @@ int main() {
 
     std::ostringstream doc_stream;
     doc_stream << doc;
-    std::string stringStream = doc_stream.str();
-    cout << stringStream << endl;
-    string renderingCommand =
-            "cat << EOF \n " + stringStream +
+    std::string string_stream = doc_stream.str();
+    cout << string_stream << endl;
+    string rendering_command =
+            "cat << EOF \n " + string_stream +
             "\n EOF |   xalan -xsl transformata_hl7/narrative-block-1.3.1/CDA_PL_IG_1.3.1.xsl -out out.html";
 
-    cout << "command " << renderingCommand << endl;
+    cout << "command " << rendering_command << endl;
 
-    system(renderingCommand.c_str());
+    system(rendering_command.c_str());
     cout << "HL7 message converted to HTMl" << endl;
 
     return 0;
